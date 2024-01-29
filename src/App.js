9ae44396b1a185c39d60cd503a44527a824bd4cb
rocksteadykirk <120696023+rocksteadykirk@users.noreply.github.com>
2024-01-29 15:33:13 +0900

    React Router - Defined pages/components and checked for correct routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>The React-Made Blog</h1>
-      <div id="page-body">
-        Welcome to The React-Made Blog!
+    <BrowserRouter>
+      <div className="App">
+        <h1>The React-Made Blog</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
+
   );
 }
 
