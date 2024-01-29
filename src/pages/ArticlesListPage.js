43e486eb577303764fc819d6acf0ba6a7172794f
rocksteadykirk <120696023+rocksteadykirk@users.noreply.github.com>
2024-01-29 21:43:56 +0900

    creating and linking articles list

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,6 +1,18 @@
+import articles from "./article-content";
+import { Link } from "react-router-dom";
+
 const ArticlesListPage = () => {
     return (
-        <h1>This is the Articles List page</h1>
+        <>
+            <h1>Articles</h1>
+            {articles.map(article => (
+                <Link key={article.name} className="article-list-item" to= {`/articles/${article.name}`}>   {/* Remember to add key when using methods like map */}
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0, 150)}...</p>  {/* substring(0, 150) For displaying a sample of content*/}
+                </Link>
+            ))}
+        </>
+
     );
 }
 
