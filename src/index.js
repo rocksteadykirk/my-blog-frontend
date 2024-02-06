7ae6671f2caed1ff041ed0381ee84297fe62b54f
rocksteadykirk <120696023+rocksteadykirk@users.noreply.github.com>
2024-02-06 15:11:53 +0900

    Integrated Google Firebase for authentication, Created component for custom hooks to manage users, Created CreateAccountPage and LoginPage

--- src/index.js
+++ src/index.js
@@ -4,6 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Start of code from google firebase authentication
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjBfAJ9aucGSZGEDFM1CWRWg7Vs7mt4IA",
+  authDomain: "my-react-blog-kip-dev.firebaseapp.com",
+  projectId: "my-react-blog-kip-dev",
+  storageBucket: "my-react-blog-kip-dev.appspot.com",
+  messagingSenderId: "794623195857",
+  appId: "1:794623195857:web:83662b60b4b38260ea63f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);   
+
+//end of code from google firebase authentication
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
