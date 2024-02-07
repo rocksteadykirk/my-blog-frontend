4c72e4a15acb850a1b2f3c1695fe542b81d27334
rocksteadykirk <120696023+rocksteadykirk@users.noreply.github.com>
2024-02-07 11:31:15 +0900

    Created custom hook useUser to track user log in state. Modified LoginPage to handle log in and modified ArticlePage to endure that only logged in users can upvote or comment.

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -5,11 +5,14 @@ import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
 import CommentsList from '../components/CommentsList';
 import AddCommentForm from '../components/AddCommentForm';
+import useUser from '../hooks/useUser';
 
 
 const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
     const { articleId } = useParams();
+
+    const { user, isLoading } = useUser();
 
     useEffect(() => {
         const loadArticleInfo = async () => {
@@ -38,16 +41,24 @@ const ArticlePage = () => {
         <>
             <h1>{article.title}</h1>
             <div className='upvotes-section'>
-                <button onClick={addUpvote}>Upvote</button>
+                {user
+                    ?<button onClick={addUpvote}>Upvote</button>
+                    :<button>Log in to upvote</button>
+                }
                 <p>This article has {articleInfo.upvotes} upvote(s)</p>
             </div>
             {article.content.map((paragraph,i) => (
                 <p key={i}>{paragraph}</p>                  
             ))}
-            <AddCommentForm 
-                articleName={articleId}
-                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}  
-            />
+
+            {user 
+                ? <AddCommentForm 
+                    articleName={articleId}
+                    onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}  
+                  />
+                : <button>Log in to add a comment</button> 
+            }
+
             <CommentsList comments={articleInfo.comments} />
         </>
 
